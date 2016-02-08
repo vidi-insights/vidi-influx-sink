2bc35b6d1549786d890a66a7ea59a01dc078c372
Dean McDonnell <mcdonnelldean@outlook.com>
2016-02-08 14:35:30 +0000

    Default rig to docker ip

--- test/rig.js
+++ test/rig.js
@@ -1,11 +1,12 @@
 'strict'
 
 const config = {
-  stats: {collector: true, log_metrics: false}
+  stats: {collector: true},
+  influx_store: {influx: {host: '192.168.99.100'}}
 }
 
 require('seneca')()
   .use('stats', config.stats)
   .use('toolbag-stats')
-  .use('..')
+  .use('..', config.influx_store)
   .listen()
